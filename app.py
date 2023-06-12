1bbd0626db7c378ca4d614d82cd5f19e12508a05
MeganSouthall <134061814+MeganSouthall@users.noreply.github.com>
2023-06-12 15:55:52 +0100

    loading data to save while pulling from genAI branch

--- app.py
+++ app.py
@@ -31,3 +31,59 @@ def upload_document():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#####################################################
+@app.route('/upload_document', methods=["POST"])
+def upload_document():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    document_path = os.path.join("..", "storage", "MJ6AP276WPV8BEZN", "documents", filename)
+    uploaded_file.save(open(document_path, "wb"))
+    return render_template("home.html")
+
+< div
+
+
+class ="card" style="height: 80%; text-align: center; background-color: #ececec" onclick="uploadFile()" >
+
+< p > < i
+
+
+class ="fa-solid fa-plus fa-2x" style="color: white" > < / i > < / p >
+
+< h6 > Add
+File < / h6 >
+< input
+type = "file"
+id = "fileInput"
+style = "display: none;" >
+< p > < small > (or drag & drop) < / small > < / p >
+
+function uploadFile() {
+            document.getElementById('fileInput').click();
+        }
+
+        document.getElementById('fileInput').addEventListener('change', function() {
+            let file = this.files[0];
+            let formData = new FormData();
+            formData.append('file', file);
+
+            fetch('/upload_document', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.text())
+                .then(data => {
+                    console.log(data);
+                    alert('File uploaded successfully');
+                    window.location.reload();
+                })
+                .catch(error => {
+                    console.error(error);
+                    alert('Error uploading file');
+                })
+        })
+
+
+cursor: pointer;
+#####################################################
